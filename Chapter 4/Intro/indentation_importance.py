ee81c40a8e75b73a27f5ba8e265c2e3eaf45c87d
DESKTOP-42MV8LH\PC <rtm.tsalisher@gmail.com>
2024-11-30 23:59:31 +0000

    chapter 4 - no exercises

---
+++ Chapter 4/Intro/indentation_importance.py
@@ -0,0 +1,7 @@
+i = 1
+while(i < 10):
+    # start of while block code
+    print(f"i = {i}")
+    i += 1
+    # end of block
+    print(f"this is line no. {i}")
---
+++ Chapter 4/Intro/self_arithmetic.py
@@ -0,0 +1,15 @@
+a = 1
+a += 2 # a = a + 2
+
+print(f"a = {a}") # a = 3
+
+b = 3
+b -= 2 # b = b - 2
+print(f"b = {b}") # b = 1
+
+c = 1
+c *= 2 # c = c*2 = 2
+print(f"c = {c}") # c = 2
+
+
+
